017d6b511bf9577a721110ecea88ab86d83e3529
Mohamed hany <mhmdalqshlan2@gmail.com>
2023-03-16 11:40:44 -0700

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
-    char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = 59;
-
-    write(2, str, len);
-
-    return 1;
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
 }
